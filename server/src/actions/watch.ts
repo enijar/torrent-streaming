a44b32f00081ed0b0ab043b828c29472626107f7
Enijar <TheEnijar@gmail.com>
2022-05-28 11:03:01 +0100

    chore: clean up watch action

--- server/src/actions/watch.ts
+++ server/src/actions/watch.ts
@@ -1,16 +1,18 @@
 import type { Response } from "express";
 import * as WebTorrent from "webtorrent";
-import * as parseTorrent from "parse-torrent";
 import * as rangeParser from "range-parser";
 import * as pump from "pump";
 import type { PrivateRequest } from "../types";
 import Stream from "../entities/stream";
-import config from "../config";
+import torrent from "../services/torrent";
 
 type CacheData = {
   stream: Stream;
   file: WebTorrent.TorrentFile;
+  updatedAt: number;
 };
+
+const CACHE_TIME = 60 * 60 * 1000; // 1 hour
 
 const cache = new Map<string, CacheData>();
 
@@ -18,57 +20,41 @@ const client = new WebTorrent();
 
 export default async function watch(req: PrivateRequest, res: Response) {
   const { uuid } = req.params ?? {};
-  const cachedData = cache.get(uuid) ?? null;
+  let cachedData = cache.get(uuid) ?? null;
 
-  let file: WebTorrent.TorrentFile = null;
+  const now = Date.now();
 
+  // Update cache
+  if (cachedData !== null && now - cachedData.updatedAt >= CACHE_TIME) {
+    const stream = await Stream.findByPk(uuid);
+    cachedData.stream = stream;
+    cachedData.file = await torrent.findFile(client, stream);
+    cachedData.updatedAt = now;
+  }
+
+  // Create cache
   if (cachedData === null) {
     const stream = await Stream.findByPk(uuid);
-
     if (stream === null) {
       return res.status(404).end();
     }
 
-    let torrentHash: string = stream.torrents?.[0]?.hash ?? null;
-    for (let i = 0, length = stream.torrents.length; i < length; i++) {
-      const torrent = stream.torrents[i];
-      if (torrent.type === "web" || torrent.quality === "1080p") {
-        torrentHash = torrent.hash;
-        break;
-      }
-    }
-
-    const parsedLink = parseTorrent(stream.torrents[0].hash);
-    const magnetUri = parseTorrent.toMagnetURI({
-      ...parsedLink,
-      announce: [...parsedLink.announce, ...config.torrentTrackers],
-    });
-
-    file = await new Promise((resolve) => {
-      client.add(magnetUri, (torrent) => {
-        const file = torrent.files.find((file) => file.name.endsWith(".mp4"));
-        resolve((file ?? null) as WebTorrent.TorrentFile);
-      });
-    });
-
+    const file = await torrent.findFile(client, stream);
     if (file === null) {
       return res.status(404).end();
     }
 
-    cache.set(stream.uuid, { stream, file });
-  } else {
-    const cachedData = cache.get(uuid);
-    file = cachedData.file;
+    cache.set(stream.uuid, { stream, file, updatedAt: Date.now() });
+    cachedData = cache.get(stream.uuid);
   }
 
-  res.attachment(file.name);
+  res.attachment(cachedData.file.name);
   res.setHeader("Accept-Ranges", "bytes");
 
   const headerRange = req.headers.range;
-  const fileSize = file.length;
-
+  const fileSize = cachedData.file.length;
   const parsedRange = headerRange ? rangeParser(fileSize, headerRange) : null;
-  const range = parsedRange instanceof Array ? parsedRange[0] : null;
+  const range = Array.isArray(parsedRange) ? parsedRange[0] : null;
 
   if (range !== null) {
     const bytes = `bytes ${range.start}-${range.end}/${fileSize}`;
@@ -83,7 +69,7 @@ export default async function watch(req: PrivateRequest, res: Response) {
     return res.end();
   }
 
-  pump(file.createReadStream(range), res, () => {
-    file.deselect();
+  pump(cachedData.file.createReadStream(range), res, () => {
+    cachedData.file.deselect();
   });
 }
---
+++ server/src/services/torrent.ts
@@ -0,0 +1,29 @@
+import * as WebTorrent from "webtorrent";
+import * as parseTorrent from "parse-torrent";
+import Stream from "../entities/stream";
+import config from "../config";
+
+const torrent = {
+  findFile(
+    client: WebTorrent.Instance,
+    stream: Stream
+  ): Promise<WebTorrent.TorrentFile> {
+    return new Promise(async (resolve) => {
+      const parsedLink = parseTorrent(stream.torrents[0].hash);
+      const magnetUri = parseTorrent.toMagnetURI({
+        ...parsedLink,
+        announce: [...parsedLink.announce, ...config.torrentTrackers],
+      });
+
+      client.add(magnetUri, (torrent) => {
+        const file = torrent.files.find((file) => {
+          return file.name.endsWith(".mp4");
+        });
+
+        resolve((file ?? null) as WebTorrent.TorrentFile);
+      });
+    });
+  },
+};
+
+export default torrent;
