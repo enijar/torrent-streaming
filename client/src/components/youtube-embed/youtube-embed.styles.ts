5209e08fe0c50eb86c7fb25199146a8800492289
Enijar <TheEnijar@gmail.com>
2022-05-29 15:58:37 +0100

    feat: add link to stream trailer on youtube

--- client/src/components/youtube-embed/youtube-embed.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const YoutubeEmbedWrapper = styled.div`
-  iframe {
-    border: none;
-    width: 100%;
-    aspect-ratio: 16 / 9;
-  }
-`;
--- client/src/pages/stream/stream.styles.ts
+++ client/src/pages/stream/stream.styles.ts
@@ -28,6 +28,15 @@ export const StreamSynopsis = styled.div`
   }
 `;
 
+export const StreamTrailer = styled.a`
+  display: flex;
+  gap: 1ch;
+
+  svg {
+    max-width: 5em;
+  }
+`;
+
 export const StreamWrapper = styled.main`
   padding: 1em;
   display: grid;
