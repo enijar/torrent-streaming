c5f7af7171aaa5ffc0c5d07108526563c3610835
Enijar <TheEnijar@gmail.com>
2023-04-10 14:28:04 +0100

    fix: disconnect from socket server

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -100,6 +100,7 @@ export default function Login() {
     return () => {
       socket.off("connect", onConnect);
       socket.off("authToken", onAuthToken);
+      socket.disconnect();
     };
   }, []);
 
