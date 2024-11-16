f5b0ffeb5fd1c9020fe7184c4e8ff4c98b5a3900
Enijar <TheEnijar@gmail.com>
2024-11-16 14:27:49 +0000

    fix: streaming

--- server/src/actions/watch.ts
+++ server/src/actions/watch.ts
@@ -1,86 +1,50 @@
 import type { Context } from "hono";
-import { stream as streaming } from "hono/streaming";
-import * as path from "node:path";
-import * as fs from "node:fs";
 import WebTorrent from "webtorrent";
-import rangeParser from "range-parser";
-import torrent from "@/services/torrent.js";
+import torrentService from "@/services/torrent-service.js";
 import Stream from "@/entities/stream.js";
 import User from "@/entities/user.js";
-import config from "@/config.js";
 
 const client = new WebTorrent();
-
-async function addStreamToUserWatchList(uuid: string, user: User) {
-  const streams = user.streams ?? [];
-  if (!streams.includes(uuid)) {
-    await user.update({ streams: [...streams, uuid] });
-  }
-}
 
 export default async function watch(ctx: Context, user: User) {
   const uuid = ctx.req.param("uuid") ?? "";
 
   const stream = await Stream.findByPk(uuid);
   if (stream === null) {
-    return ctx.status(404);
+    ctx.status(404);
+    return ctx.text("404");
   }
 
-  const file = await torrent.findFile(client, stream);
-  if (file === null) {
-    return ctx.status(404);
+  // Add stream to user watch list
+  const streams = user?.streams ?? [];
+  if (!streams.includes(uuid)) {
+    await user.update({ streams: [...streams, uuid] });
   }
 
-  await addStreamToUserWatchList(uuid, user);
-
-  const filePath = path.join(config.paths.torrents, file.path);
-  const stat = await fs.promises.stat(filePath);
-  const fileSize = stat.size;
-
-  const range = ctx.req.header("range");
-
-  if (range === undefined) {
-    ctx.status(422);
-    return ctx.text("Missing Range header");
+  const magnetUri = await torrentService.parse(stream.torrents);
+  if (magnetUri === null) {
+    ctx.status(404);
+    return ctx.body(null);
   }
 
-  const ranges = rangeParser(fileSize, range);
-
-  if (ranges === -1 || ranges === -2) {
-    // Invalid range, Range Not Satisfiable
-    ctx.header("Content-Range", `bytes */${fileSize}`);
-    return ctx.status(416);
-  }
-
-  const { start, end } = ranges[0];
-
-  // Partial Content
-  ctx.status(206);
-  ctx.header("Content-Range", `bytes ${start}-${end}/${fileSize}`);
-  ctx.header("Accept-Ranges", "bytes");
-  ctx.header("Content-Length", String(end - start + 1));
-  ctx.header("Content-Type", "video/mp4");
-
-  const readStream = fs.createReadStream(filePath, { start, end });
-
-  return streaming(ctx, async (stream) => {
-    // Handle stream abortion if needed
-    stream.onAbort(() => {
-      readStream.removeAllListeners();
-      readStream.destroy();
-    });
-    try {
-      // Asynchronously iterate over the Node.js Readable stream
-      for await (const chunk of readStream) {
-        // Write each chunk to the Hono stream
-        await stream.write(chunk);
-      }
-      // Signal that streaming is complete
-      await stream.close();
-    } catch {
-      readStream.removeAllListeners();
-      readStream.destroy();
-      stream.abort();
+  try {
+    const stream = await torrentService.stream(client, magnetUri, ctx.req.header("Range") ?? "");
+    if (stream === null || ctx.req.method === "HEAD") {
+      ctx.status(200);
+      return ctx.body(null);
     }
-  });
+    return new Response(stream.readStream, {
+      status: 206,
+      headers: {
+        "Accept-Ranges": "bytes",
+        "Content-Type": "video/mp4",
+        "Content-Length": `${stream.fileSize}`,
+        "Content-Range": `bytes ${stream.range.start}-${stream.range.end}/${stream.fileSize}`,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    ctx.status(500);
+    return ctx.text("server error");
+  }
 }
