907e2391135a1e26edbd1b06998008080b8b15c9
Enijar <TheEnijar@gmail.com>
2024-12-14 12:17:41 +0000

    fix: run cron

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import database from "@/services/database.js";
 import config from "@/config.js";
 import "@/services/server.js";
+import cron from "@/services/cron.js";
 
 database
   .sync({ alter: true })
   .then(() => {
+    cron();
     console.log(`Server is running on http://localhost:${config.port}`);
   })
   .catch((err) => {
