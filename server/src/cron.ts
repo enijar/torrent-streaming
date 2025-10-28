bdf6046dd1cecb234787ee2930a5e1c58dad2e2b
Enijar <TheEnijar@gmail.com>
2025-10-28 21:32:40 +0000

    fix: reload server at 2AM daily

--- server/src/cron.ts
+++ server/src/cron.ts
@@ -1,3 +1,4 @@
+import { execSync } from "node:child_process";
 import { schedule } from "node-cron";
 import updateMovies from "~/services/update-movies.js";
 import deleteOldTorrentFiles from "~/services/delete-old-torrent-files.js";
@@ -13,4 +14,10 @@ schedule("0 */6 * * *", async () => {
   console.log(`[cron: start] delete-old-torrent-files`);
   await deleteOldTorrentFiles();
   console.log(`[cron: end] delete-old-torrent-files`);
+});
+// 2AM daily
+schedule("0 2 * * *", async () => {
+  console.log(`[cron: start] restart`);
+  execSync(`pm2 reload torrent-streaming`, { stdio: "inherit" });
+  console.log(`[cron: end] restart`);
 });
