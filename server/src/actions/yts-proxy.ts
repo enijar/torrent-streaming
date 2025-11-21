d042d1d8bff7043b31aa39a52556bad6b9c12148
Enijar <TheEnijar@gmail.com>
2025-11-21 19:37:48 +0000

    fix: ytsProxy middleware

--- server/src/actions/yts-proxy.ts
+++ server/src/actions/yts-proxy.ts
@@ -1,7 +1,12 @@
 import type { Context } from "hono";
 
 export default async function ytsProxy(ctx: Context) {
-  const url = new URL("https://yts.lt");
+  const url = new URL("https://img.yts.lt");
   url.pathname = ctx.req.param("url") ?? "/";
-  return fetch(url);
+  const response = await fetch(url);
+  return new Response(Buffer.from(await response.arrayBuffer()), {
+    headers: {
+      "Content-Type": response.headers.get("Content-Type") ?? "",
+    },
+  });
 }
