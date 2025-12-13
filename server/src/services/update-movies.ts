df1467c12cf989d1aa701030c963f41e739745ff
Enijar <TheEnijar@gmail.com>
2025-12-13 11:44:07 +0000

    chore: default maxPages to infinity for update-movies command

--- server/src/services/update-movies.ts
+++ server/src/services/update-movies.ts
@@ -44,7 +44,7 @@ export default async function updateMovies() {
   const progress = new SingleBar({}, Presets.shades_classic);
   const limit = 50;
   const concurrent = 10;
-  const maxPages = 200;
+  const maxPages = Infinity;
   const url = new URL("https://yts.lt/api/v2/list_movies.json");
   url.searchParams.set("sort", "download_count");
   url.searchParams.set("quality", "1080p");
